15d03faf65179ec48f14803d67430cd0c0366077
Ianis Tutunaru <129986472+iantutunaru@users.noreply.github.com>
2026-03-18 17:00:21 +0200

    *Added "Launch Pad" prefab, ability to boost the rocket up, and RigidBody and Box Collider to the player

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,10 +3,28 @@ using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
+    [Header("References")]
     [SerializeField] private InputAction thrust;
-
+    [SerializeField]private Rigidbody rb;
+    
+    [Header("Variables")]
+    [SerializeField] private float moveSpeed = 10f;
+    
     private void OnEnable()
     {
         thrust.Enable();
+    }
+
+    private void FixedUpdate()
+    {
+        if (thrust.IsPressed())
+        {
+            rb.AddRelativeForce(Vector3.up * (moveSpeed * Time.fixedDeltaTime));
+        }
+    }
+    
+    private void OnDisable()
+    {
+        thrust.Disable();
     }
 }
